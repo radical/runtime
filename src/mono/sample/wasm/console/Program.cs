d966fa56cae2d5d052e1e6029d42cdc1bc23e6b1
Ankit Jain <radical@gmail.com>
2021-05-04 19:47:46 -0400

    all tests passing

--- src/mono/sample/wasm/console/Program.cs
+++ src/mono/sample/wasm/console/Program.cs
@@ -2,19 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Text;
+using System.Threading.Tasks;
 
 public class Test
 {
-    public static int Main(string[] args)
+    public static async Task<int> Main(string[] args)
     {
-        var sb = new StringBuilder();
-        sb.Append("123");
-        //await Task.Delay(1);
-        Console.WriteLine($"Hello World: {sb}!");
-        //for (int i = 0; i < args.Length; i++) {
-            //Console.WriteLine($"args[{i}] = {args[i]}");
-        //}
-        return 42 + sb.Length;
+        await Task.Delay(1);
+        Console.WriteLine("Hello World - jsut !");
+        for (int i = 0; i < args.Length; i++) {
+            Console.WriteLine($"args[{i}] = {args[i]}");
+        }
+        return args.Length;
     }
 }
